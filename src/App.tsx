ab6be50e5e34c930f8690b89da8c552a8ffc17c7
Aborisade <bobleeswaggs123@gmail.com>
2024-04-25 15:49:11 +0200

    First step to re-writing a project with typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Helo rhis is me rewritin my code, soft right??
+      Yeah it isyd 
+    </div>
+  );
+}
+
+export default App;
